8623dded289b58b76bd12ad07fccd2532fa11916
Rafael Rey <reyarcenegui@gmail.com>
2021-07-09 12:34:20 +0200

    removed unused lib

--- src/ROS/planner_ros_node.cpp
+++ src/ROS/planner_ros_node.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 #include "Planners/AStarGenerator.hpp"
-#include "Planners/ParallelAStarGenerator.hpp"
 #include "Planners/CostAwareAStarGenerator.hpp"
 #include "Planners/ThetaStarGenerator.hpp"
 #include "Planners/LazyThetaStarGenerator.hpp"
