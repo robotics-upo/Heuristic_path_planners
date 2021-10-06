50f89d100b8fbf03776d17302100f9473eb1a9ad
Rafael Rey <reyarcenegui@gmail.com>
2021-10-06 17:01:43 +0200

    Version 0.3.0: New Planners added

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-   packages=['heuristic_planners'],
-   package_dir={'': 'scripts'}
+   # packages=['heuristic_planners'],
+   # package_dir={'': 'scripts'}
 )
 
 setup(**setup_args)
